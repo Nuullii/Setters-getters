901a764f7ffd969a9ade81077b3d7a4feee61c5f
Nuullii <154333896+Nuullii@users.noreply.github.com>
2024-01-18 13:43:12 +0100

    Update Main.java
    cambio de variables en la creacion del objeto ordenador

--- ED 2.0/src/Main.java
+++ ED 2.0/src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 public class Main {
     public static void main(String[] args) {
 
-        Ordenador pc= new Ordenador("i3", "RTX", 16);
+        Ordenador pc= new Ordenador("i4", "GTX", 128);
 
         Ordenador pc1 =new Ordenador();
         System.out.println(pc.getClass());
